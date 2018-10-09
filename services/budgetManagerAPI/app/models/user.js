92d8f12cd0b7e6b1db0fe250346f2a53d1038aba
sadokaah <radupasparuga25@gmail.com>
2018-10-09 18:56:00 +0300

    added index.js and user.js, still didn't solve the bcrypt bug

---
+++ services/budgetManagerAPI/app/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'),
+const Schema = mongoose.Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    clients: [{}]
+  });
