7bff4e8def58fff45bdbb59dd8952a4f29edecbf
sadokaah <radupasparuga25@gmail.com>
2018-10-11 19:57:31 +0300

    integrated vue, added a bg and some basic setup with the authentication component

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// Pages
+import Authentication from '@/components/pages/Authentication/Authentication'
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'Authentication',
+      component: Authentication
+    }
+  ]
+})
